74844f030202d589a8e39de2b0edf1010edb5587
vishaag <vishaag.s@gmail.com>
2016-04-01 00:10:05 +0530

    Added DB Boilerplate

--- app.py
+++ app.py
@@ -1,44 +1,36 @@
 #!flask/bin/python
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
+from flask.ext.sqlalchemy import SQLAlchemy
+import json
+import os
 
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
-menu = [
-    {
-        'id': 1,
-        'item': u'Chappathi Channa',
-        'price': u'10+20'
-    },
-    {
-        'id': 2,
-        'item': u'Apple MilkShake',
-        'price': u'50'
-    },
 
-    {
-        'id': 3,
-        'item': u'Namkeen',
-        'price': u'10'
-    },
-    {
-        'id': 4,
-        'item': u'Lays',
-        'price': u'10'
-    }
-]
 
-# @app.route('/amritatoday/api/v1.0/menu', methods=['GET'])
-# def get_tasks():
-#     return jsonify({'tasks': tasks})
+class Menu(db.Model):
+    __tablename__ = "menu"
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(20), unique=True)
+    price = db.Column(db.Integer)
 
-class Menu(Resource):
+
+class MenuList(Resource):
     def get(self):
-        return jsonify({'menu': menu})
+        menu=[]
+        for u in Menu.query.all():
+            menu.append(u.__dict__)
+        for i in range(len(menu)):
+            del menu[i]['_sa_instance_state'] #Delete uncessary parameter in return object
+        return jsonify({'menu':(menu) })
 
-api.add_resource(Menu,'/api/v1.0/menu')
+
+api.add_resource(MenuList,'/api/v1.0/menu')
 
 if __name__ == '__main__':
     app.run(debug=True)
