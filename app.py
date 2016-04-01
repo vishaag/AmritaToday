235d8352ba303cc3fa3bb25dac886a2e0fa5cb3a
vishaag <vishaag.s@gmail.com>
2016-04-01 19:20:11 +0530

    Canteen RESTing done

--- app.py
+++ app.py
@@ -45,11 +45,12 @@ class IT(db.Model):
 
 
 ##########################################################################################
-# HTTP verb definitions #
+# HTTP verbs' definitions #
 ##########################################################################################
 
+#GET (LIST) and POST ONLY
 
-class MainMenu(Resource):
+class MainMenuListAPI(Resource):
     def get(self):
         Menu=[]
         for u in Main.query.all():
@@ -68,22 +69,7 @@ class MainMenu(Resource):
         db.session.commit()
         return 201
 
-    # def put(self):
-    #     data = request.json
-    #     item = data['item']
-    #     price = data['price']
-    #     category = data['category']     
-    #     MainMenuUpdate = Menu.query.filter_by(item=item,price=price,category=category).first()
-
-    def delete(self):
-        data=request.json
-        id = data['id']
-        Main.query.filter_by(id=id).delete()
-        db.session.commit()
-        return 201 #Remember to return error code if entry is NOT deleted from DB
-
-
-class MBAMenu(Resource):
+class MBAMenuListAPI(Resource):
     def get(self):
         Menu=[]
         for u in MBA.query.all():
@@ -102,15 +88,7 @@ class MBAMenu(Resource):
         db.session.commit()
         return 201
 
-    def delete(self):
-        data=request.json
-        id = data['id']
-        MBA.query.filter_by(id=id).delete()
-        db.session.commit()
-        return 201 #Remember to return error code if entry is NOT deleted from DB
-
-
-class ITMenu(Resource):
+class ITMenuListAPI(Resource):
     def get(self):
         Menu=[]
         for u in IT.query.all():
@@ -129,9 +107,74 @@ class ITMenu(Resource):
         db.session.commit()
         return 201
 
-    def delete(self):
+
+
+#GET (ID) and PUT,DELETE
+
+class MainMenuAPI(Resource):
+    def get(self,id):
+        Menu=[]
+        Menu.append(Main.query.filter_by(id=id).first().__dict__)
+        del Menu[0]['_sa_instance_state'] #Delete uncessary parameter in return object
+        return jsonify({'Main':(Menu) })
+
+    def put(self,id):    
         data=request.json
-        id = data['id']
+        MainMenuUpdate = Main.query.filter_by(id=id).first()
+        MainMenuUpdate.item = data['item']
+        MainMenuUpdate.price = data['price']
+        MainMenuUpdate.category = data['category']
+        db.session.add(MainMenuUpdate)
+        db.session.commit()
+        return 201
+
+    def delete(self,id):
+        Main.query.filter_by(id=id).delete()
+        db.session.commit()
+        return 201 #Remember to return error code if entry is NOT deleted from DB
+
+
+class MBAMenuAPI(Resource):
+    def get(self,id):
+        Menu=[]
+        Menu.append(MBA.query.filter_by(id=id).first().__dict__)
+        del Menu[0]['_sa_instance_state'] #Delete uncessary parameter in return object
+        return jsonify({'MBA':(Menu) })
+
+    def put(self,id):    
+        data=request.json
+        MBAMenuUpdate = MBA.query.filter_by(id=id).first()
+        MBAMenuUpdate.item = data['item']
+        MBAMenuUpdate.price = data['price']
+        MBAMenuUpdate.category = data['category']
+        db.session.add(MBAMenuUpdate)
+        db.session.commit()
+        return 201
+
+    def delete(self,id):
+        MBA.query.filter_by(id=id).delete()
+        db.session.commit()
+        return 201 #Remember to return error code if entry is NOT deleted from DB
+
+
+class ITMenuAPI(Resource):
+    def get(self,id):
+        Menu=[]
+        Menu.append(IT.query.filter_by(id=id).first().__dict__)
+        del Menu[0]['_sa_instance_state'] #Delete uncessary parameter in return object
+        return jsonify({'IT':(Menu) })
+
+    def put(self,id):    
+        data=request.json
+        ITMenuUpdate = IT.query.filter_by(id=id).first()
+        ITMenuUpdate.item = data['item']
+        ITMenuUpdate.price = data['price']
+        ITMenuUpdate.category = data['category']
+        db.session.add(ITMenuUpdate)
+        db.session.commit()
+        return 201
+
+    def delete(self,id):
         IT.query.filter_by(id=id).delete()
         db.session.commit()
         return 201 #Remember to return error code if entry is NOT deleted from DB
@@ -141,10 +184,15 @@ class ITMenu(Resource):
 # Routes #
 ##########################################################################################
 
-api.add_resource(MainMenu,'/api/v1.0/menu/main')
-api.add_resource(MBAMenu,'/api/v1.0/menu/mba')
-api.add_resource(ITMenu,'/api/v1.0/menu/it')
+#GET (LIST), POST 
+api.add_resource(MainMenuListAPI,'/api/v1.0/menu/main')
+api.add_resource(MBAMenuListAPI,'/api/v1.0/menu/mba')
+api.add_resource(ITMenuListAPI,'/api/v1.0/menu/it')
 
+#GET (ID), PUT, DELETE
+api.add_resource(MainMenuAPI,'/api/v1.0/menu/main/<int:id>')
+api.add_resource(MBAMenuAPI,'/api/v1.0/menu/mba/<int:id>')
+api.add_resource(ITMenuAPI,'/api/v1.0/menu/it/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
